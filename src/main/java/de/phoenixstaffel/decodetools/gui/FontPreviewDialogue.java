b0f1c67b4dd225b808fc983c62162b0022816423
SydMontague <sydmontague@phoenix-staffel.de>
2017-10-14 19:36:18 +0200

    Improved KPTF Preview Window
    Exposed line height, scale and start position to the UI.
    Draws a textbox on the background.
    Preview window scales to fit the text window.

--- src/main/java/de/phoenixstaffel/decodetools/gui/FontPreviewDialogue.java
+++ src/main/java/de/phoenixstaffel/decodetools/gui/FontPreviewDialogue.java
@@ -7,6 +7,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JSpinner;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.border.BevelBorder;
 
 import de.phoenixstaffel.decodetools.res.payload.GMIOPayload;
@@ -15,6 +16,7 @@ import de.phoenixstaffel.decodetools.res.payload.TNFOPayload;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import java.awt.Font;
 import java.util.List;
+import javax.swing.SpinnerNumberModel;
 
 public class FontPreviewDialogue extends JFrame {
     private TNFOPayload tnfo;
@@ -27,11 +29,24 @@ public class FontPreviewDialogue extends JFrame {
     private final JLabel lblFontSize = new JLabel("Font Size:");
     private final JLabel lblWidespace = new JLabel("Widespace:");
     private final JKPTFText text = new JKPTFText();
+    private final JLabel lblStartY = new JLabel("Start Y");
+    private final JLabel lblFontColor = new JLabel("Font Color");
+    private final JLabel lblBgColor = new JLabel("BG Color");
+    private final JLabel lblStartX = new JLabel("Start X");
+    private final JLabel lblScale = new JLabel("Res. Scale");
+    private final JLabel lblLineHeight = new JLabel("Line Height");
+    private final JSpinner lineHeightSpinner = new JSpinner();
+    private final JSpinner scaleSpinner = new JSpinner();
+    private final JSpinner startXSpinner = new JSpinner();
+    private final JSpinner startYSpinner = new JSpinner();
+    private final JTextField fontColorField = new JTextField(); //TODO change into something with color
+    private final JTextField bgcolorField = new JTextField(); //TODO change into something with color
     
     public FontPreviewDialogue() {
         setResizable(false);
         setDefaultCloseOperation(HIDE_ON_CLOSE);
         setBounds(0, 0, 600, 400);
+        
         inputText.setFont(new Font("Monospaced", Font.PLAIN, 13));
         inputText.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
         inputText.setTabSize(4);
@@ -43,12 +58,27 @@ public class FontPreviewDialogue extends JFrame {
             text.setText(inputText.getText());
             text.setFontSize((int) fontSizeSpinner.getValue());
             text.setWidespace((int) widespaceSpinner.getValue());
+            text.setLineHeight((int) lineHeightSpinner.getValue());
+            text.setResolutionScale((double) scaleSpinner.getValue());
+            text.setStartX((int) startXSpinner.getValue());
+            text.setStartY((int) startYSpinner.getValue());
             text.setTNFO(tnfo);
             text.setGMIOs(gmios);
             text.update();
+            
+            pack();
         }));
         
         setFormat();
+
+        lblStartY.setLabelFor(startYSpinner);
+        lblStartX.setLabelFor(startXSpinner);
+        lblScale.setLabelFor(scaleSpinner);
+        lblLineHeight.setLabelFor(lineHeightSpinner);
+        lblWidespace.setLabelFor(widespaceSpinner);
+        lblFontSize.setLabelFor(fontSizeSpinner);
+        
+        pack();
     }
     
     public void setTNFO(TNFOPayload tnfo) {
@@ -67,47 +97,84 @@ public class FontPreviewDialogue extends JFrame {
                 .addGroup(groupLayout.createSequentialGroup()
                     .addContainerGap()
                     .addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-                        .addComponent(text, GroupLayout.PREFERRED_SIZE, 558, GroupLayout.PREFERRED_SIZE)
+                        .addComponent(inputText, GroupLayout.PREFERRED_SIZE, 400, GroupLayout.PREFERRED_SIZE)
                         .addGroup(groupLayout.createSequentialGroup()
-                            .addComponent(inputText, GroupLayout.PREFERRED_SIZE, 415, GroupLayout.PREFERRED_SIZE)
                             .addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
                                 .addGroup(groupLayout.createSequentialGroup()
-                                    .addGap(10)
                                     .addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-                                        .addComponent(lblWidespace)
-                                        .addComponent(lblFontSize))
+                                        .addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+                                            .addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+                                                .addComponent(lblWidespace)
+                                                .addComponent(lblFontSize)
+                                                .addComponent(lblLineHeight, GroupLayout.PREFERRED_SIZE, 67, GroupLayout.PREFERRED_SIZE))
+                                            .addComponent(lblStartX, GroupLayout.PREFERRED_SIZE, 67, GroupLayout.PREFERRED_SIZE))
+                                        .addComponent(lblScale, GroupLayout.PREFERRED_SIZE, 67, GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(lblStartY, GroupLayout.PREFERRED_SIZE, 67, GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(lblFontColor, GroupLayout.PREFERRED_SIZE, 67, GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(lblBgColor, GroupLayout.PREFERRED_SIZE, 67, GroupLayout.PREFERRED_SIZE))
                                     .addPreferredGap(ComponentPlacement.RELATED)
                                     .addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-                                        .addComponent(fontSizeSpinner, GroupLayout.DEFAULT_SIZE, 56, Short.MAX_VALUE)
-                                        .addComponent(widespaceSpinner, GroupLayout.DEFAULT_SIZE, 80, Short.MAX_VALUE)))
-                                .addGroup(groupLayout.createSequentialGroup()
-                                    .addPreferredGap(ComponentPlacement.RELATED)
-                                    .addComponent(updateButton)))))
+                                        .addComponent(fontColorField, GroupLayout.PREFERRED_SIZE, 63, GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(bgcolorField, GroupLayout.PREFERRED_SIZE, 63, GroupLayout.PREFERRED_SIZE)
+                                        .addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+                                            .addComponent(fontSizeSpinner, GroupLayout.DEFAULT_SIZE, 63, Short.MAX_VALUE)
+                                            .addComponent(lineHeightSpinner, GroupLayout.DEFAULT_SIZE, 63, Short.MAX_VALUE)
+                                            .addComponent(scaleSpinner, GroupLayout.DEFAULT_SIZE, 63, Short.MAX_VALUE)
+                                            .addComponent(startXSpinner, GroupLayout.DEFAULT_SIZE, 63, Short.MAX_VALUE)
+                                            .addComponent(startYSpinner, GroupLayout.DEFAULT_SIZE, 63, Short.MAX_VALUE)
+                                            .addComponent(widespaceSpinner))))
+                                .addComponent(updateButton))
+                            .addPreferredGap(ComponentPlacement.RELATED)
+                            .addComponent(text, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE, Short.MAX_VALUE)))
                     .addContainerGap())
         );
         groupLayout.setVerticalGroup(
             groupLayout.createParallelGroup(Alignment.LEADING)
                 .addGroup(groupLayout.createSequentialGroup()
+                    .addContainerGap()
+                    .addComponent(inputText, GroupLayout.PREFERRED_SIZE, 78, GroupLayout.PREFERRED_SIZE)
+                    .addPreferredGap(ComponentPlacement.RELATED)
                     .addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
                         .addGroup(groupLayout.createSequentialGroup()
-                            .addGap(18)
                             .addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
                                 .addComponent(lblFontSize)
                                 .addComponent(fontSizeSpinner, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-                            .addGap(7)
+                            .addPreferredGap(ComponentPlacement.RELATED)
                             .addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
                                 .addComponent(lblWidespace)
                                 .addComponent(widespaceSpinner, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
                             .addPreferredGap(ComponentPlacement.RELATED)
+                            .addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+                                .addComponent(lineHeightSpinner, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                                .addComponent(lblLineHeight))
+                            .addPreferredGap(ComponentPlacement.RELATED)
+                            .addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+                                .addComponent(scaleSpinner, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                                .addComponent(lblScale))
+                            .addPreferredGap(ComponentPlacement.RELATED)
+                            .addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+                                .addComponent(startXSpinner, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                                .addComponent(lblStartX))
+                            .addGap(6)
+                            .addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+                                .addComponent(startYSpinner, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                                .addComponent(lblStartY))
+                            .addPreferredGap(ComponentPlacement.RELATED)
+                            .addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+                                .addComponent(fontColorField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                                .addComponent(lblFontColor))
+                            .addPreferredGap(ComponentPlacement.RELATED)
+                            .addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+                                .addComponent(bgcolorField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                                .addComponent(lblBgColor))
+                            .addPreferredGap(ComponentPlacement.RELATED)
                             .addComponent(updateButton))
-                        .addGroup(groupLayout.createSequentialGroup()
-                            .addContainerGap()
-                            .addComponent(inputText, GroupLayout.PREFERRED_SIZE, 78, GroupLayout.PREFERRED_SIZE)))
-                    .addGap(12)
-                    .addComponent(text, GroupLayout.DEFAULT_SIZE, 242, Short.MAX_VALUE)
+                        .addComponent(text, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE, Short.MAX_VALUE))
                     .addContainerGap())
         );
         getContentPane().setLayout(groupLayout);
         //@formatter:on
+        
+        scaleSpinner.setModel(new SpinnerNumberModel(new Double(1), 0D, null, new Double(1)));
     }
 }
--- src/main/java/de/phoenixstaffel/decodetools/gui/JKPTFText.java
+++ src/main/java/de/phoenixstaffel/decodetools/gui/JKPTFText.java
@@ -1,13 +1,18 @@
 package de.phoenixstaffel.decodetools.gui;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.AffineTransform;
 import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
+import javax.annotation.Resource;
+import javax.imageio.ImageIO;
 import javax.swing.JComponent;
 
 import de.phoenixstaffel.decodetools.res.payload.GMIOPayload;
@@ -16,15 +21,23 @@ import de.phoenixstaffel.decodetools.res.payload.TNFOPayload.TNFOEntry;
 
 public class JKPTFText extends JComponent {
     private static final int WIDTH = 400;
+    private static final int HEIGHT = 240;
+    
+    private transient BufferedImage textBoxImage;
     
     private String text = "";
     private int fontSize = 0;
     private int widespace = 0;
+    private int lineHeight = 15;
     
-    private int lineHeight = 15; //FIXME add line height to GUI
+    private int startX = 0;
+    private int startY = 0;
+    private double resolutionScale = 1;
 
     private TNFOPayload tnfo;
     private List<GMIOPayload> gmios;
+    
+    private boolean showTextbox = true;
     
     //space width in px
     //FIXME linear scaling
@@ -35,44 +48,63 @@ public class JKPTFText extends JComponent {
     
     //widespace -> extra space between characters, unscaled by font size!
     
+    public JKPTFText() {
+        super();
+        setPreferredSize(new Dimension((int) (WIDTH * resolutionScale), (int) (HEIGHT * resolutionScale)));
+        try {
+            textBoxImage = ImageIO.read(getClass().getClassLoader().getResource("textbox.png"));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
     public void setText(String text) {
         this.text = text;
     }
     
-    
-    
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, (int) (WIDTH * resolutionScale), (int) (HEIGHT * resolutionScale));
+        g.setColor(Color.WHITE);
         
         if(tnfo == null)
             return;
         
         Graphics2D gg = (Graphics2D) g;
+
+        if(showTextbox) {
+            AffineTransform t2 = new AffineTransform();
+            t2.scale(resolutionScale, resolutionScale);
+            t2.translate(61, 172);
+            gg.drawImage(textBoxImage, t2, null);
+        }
         
         //TODO font color
         //TODO background color
         
-        g.setColor(Color.BLACK);
-        g.fillRect(0, 0, 500, 500);
-        g.setColor(Color.WHITE);
+        double x = startX * resolutionScale;
+        int y = (int) ((lineHeight + startY) * resolutionScale);
         
-        double x = 0;
-        int y = lineHeight;
-        
-        double scale = fontSize / (double) tnfo.getReferenceSize();
+        double scale = resolutionScale * fontSize / tnfo.getReferenceSize();
 
+        if(scale == 0)
+            return;
+        
         for(char c : text.toCharArray()) {
             switch(c) {
                 case '\r':
                     break;
                 case '\n':
-                    x = 0;
-                    y += lineHeight;
+                    x = startX * resolutionScale;
+                    y += lineHeight * resolutionScale;
                     break;
                 case ' ':
                     x += tnfo.getSpaceWidth() * scale;
-                    x += widespace;
+                    x += widespace * resolutionScale;
                     break;
                 default:
                     TNFOEntry entry = tnfo.getEntry(c);
@@ -89,9 +121,12 @@ public class JKPTFText extends JComponent {
 
                         double localX = x + entry.getXTranslation() * scale;
                         double localY = y - entry.getYTranslation() * scale - scale;
+
+                        if(localY < 0)
+                            continue;
                         
-                        double textureScaleX = (double) entry.getWidth() / (x2-x1);
-                        double textureScaleY = (double) entry.getHeight() / (y1-y2);
+                        double textureScaleX = (double) entry.getWidth() / (x2 - x1);
+                        double textureScaleY = (double) entry.getHeight() / (y1 - y2);
 
                         AffineTransform t = new AffineTransform();
                         t.translate(localX, localY);
@@ -105,7 +140,7 @@ public class JKPTFText extends JComponent {
                     }
                     
                     x += entry.getTextWidth() * scale;
-                    x += widespace;
+                    x += widespace * resolutionScale;
             }
         }
     }
@@ -121,6 +156,18 @@ public class JKPTFText extends JComponent {
     public void setLineHeight(int lineHeight) {
         this.lineHeight = lineHeight;
     }
+
+    public void setResolutionScale(double value) {
+        this.resolutionScale = value;
+    }
+
+    public void setStartX(int value) {
+        this.startX = value;
+    }
+
+    public void setStartY(int value) {
+        this.startY = value;
+    }
     
     public void setTNFO(TNFOPayload tnfo) {
         this.tnfo = tnfo;
@@ -130,10 +177,8 @@ public class JKPTFText extends JComponent {
         this.gmios = gmios;
     }
 
-
     public void update() {
         repaint();
+        setPreferredSize(new Dimension((int) (WIDTH * resolutionScale), (int) (HEIGHT * resolutionScale)));
     }
-    
-    
 }
