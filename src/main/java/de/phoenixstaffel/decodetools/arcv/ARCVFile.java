de4db0d537d1e599d45df5f996891868e2b27b55
SydMontague <sydmontague@phoenix-staffel.de>
2018-11-20 16:38:07 +0100

    Refactored ARCV rebuild process to be multi-threaded

--- src/main/java/de/phoenixstaffel/decodetools/arcv/ARCVFile.java
+++ src/main/java/de/phoenixstaffel/decodetools/arcv/ARCVFile.java
@@ -1,5 +1,6 @@
 package de.phoenixstaffel.decodetools.arcv;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -18,18 +19,9 @@ import de.phoenixstaffel.decodetools.res.ResPayload;
 import de.phoenixstaffel.decodetools.res.ResPayload.Payload;
 import de.phoenixstaffel.decodetools.res.ResFile;
 
-//this is crap, but it works...
 public class ARCVFile {
     private File inputDir;
     private boolean compressed;
-    
-    private Access destination;
-    private int sectorCount = 0;
-    private VCRAFile arcvinfo;
-    
-    private long zipTime = 0;
-    private long resLoadTime = 0;
-    private long resDataTime = 0;
     
     public ARCVFile(File inputDir, boolean compressed) {
         if (!inputDir.isDirectory())
@@ -37,7 +29,6 @@ public class ARCVFile {
         
         this.inputDir = inputDir;
         this.compressed = compressed;
-        this.arcvinfo = new VCRAFile();
     }
     
     public ARCVFile(File inputDir) {
@@ -47,46 +38,60 @@ public class ARCVFile {
     public void saveFiles(File outputDir) throws IOException {
         File destFile = new File(outputDir, "ARCV0.BIN");
         
-        if (destFile.exists() && !destFile.delete())
+        if (Files.deleteIfExists(destFile.toPath()))
             return;
         if (!destFile.exists() && !destFile.createNewFile())
             return;
         
-        destination = new FileAccess(destFile);
+        Access destination = new FileAccess(destFile);
+        VCRAFile arcvinfo = new VCRAFile();
+        RunValues run = new RunValues();
         
         List<File> list = Utils.listFiles(inputDir);
         
-        list.stream().filter(a -> !a.getName().endsWith(".bak")).forEach(t -> {
-            if (t.isDirectory())
-                return;
-            
+        list.parallelStream().filter(a -> !a.getName().endsWith(".bak") && a.isFile()).map(t -> {
+            Path filePath = inputDir.toPath().relativize(t.toPath());
+            Main.LOGGER.info("Adding " + filePath);
             try {
-                Main.LOGGER.info("Adding " + inputDir.toPath().relativize(t.toPath()));
-                addFile(t.toPath(), inputDir.toPath().relativize(t.toPath()).toString().replaceAll("\\\\", "/")); // fuck windows
+                return addFile(t.toPath(), filePath.toString().replaceAll("\\\\", "/")); // fuck windows
             }
             catch (IOException e) {
-                Main.LOGGER.log(Level.WARNING, "Exception while adding file to ARCVFile.", e);
+                Main.LOGGER.log(Level.WARNING, "Exception while adding file to ARCVFile: " + filePath, e);
             }
+            
+            return null;
+        }).forEachOrdered(a -> {
+            arcvinfo.addEntry(new VCRAEntry(a.compressedSize, a.uncompressedSize, a.name, run.sectorCount, a.marv));
+            destination.writeByteArray(a.output.toByteArray());
+            run.sectorCount += (a.output.size() + 0x800 - 1) / 0x800;
+            
+            run.zipTime += a.zipTime;
+            run.resLoadTime += a.loadTime;
+            run.resDataTime += a.dataTime;
         });
         
         destination.close();
         arcvinfo.repack(new File(outputDir, "ARCVINFO.BIN"));
         
-        Main.LOGGER.info(() -> String.format("ZIP: %d | Res Load: %d | Res Data: %d", (zipTime / 1000000), (resLoadTime / 1000000), (resDataTime / 1000000)));
+        Main.LOGGER.info(() -> String.format("ZIP: %d | Res Load: %d | Res Data: %d",
+                                             (run.zipTime / 1000000),
+                                             (run.resLoadTime / 1000000),
+                                             (run.resDataTime / 1000000)));
     }
     
-    private void addFile(Path a, String name) throws IOException {
-        if (a.toFile().isDirectory())
-            return;
+    private AddFileResult addFile(Path a, String name) throws IOException {
+        AddFileResult result = new AddFileResult();
+        result.name = name;
         
-        int startSector = sectorCount;
-        int compressedSize = 0;
+        long timer = System.nanoTime();
         
-        long t = System.nanoTime();
-        
+        // ZIP
         byte[] input = Files.readAllBytes(a);
+        boolean shouldCompress = input.length > 0x1000 && compressed;
+        int compressedSize = 0;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         
-        if (input.length > 0x1000 && compressed) {
+        if (shouldCompress) {
             Deflater compresser = new Deflater(Deflater.BEST_COMPRESSION);
             compresser.setInput(input);
             compresser.finish();
@@ -94,28 +99,31 @@ public class ARCVFile {
             byte[] output = new byte[0x800];
             while (!compresser.finished()) {
                 compressedSize += compresser.deflate(output);
-                destination.writeByteArray(output);
-                sectorCount++;
+                outputStream.write(output);
             }
-            compresser.end();
         }
         else {
             byte[] output = Arrays.copyOf(input, Utils.align(input.length, 0x800));
             compressedSize = input.length;
-            destination.writeByteArray(output);
-            sectorCount += output.length / 0x800;
+            outputStream.write(output);
         }
         
-        zipTime += System.nanoTime() - t;
-        t = System.nanoTime();
+        result.output = outputStream;
+        result.compressedSize = compressedSize;
+        result.uncompressedSize = input.length;
         
+        result.zipTime = System.nanoTime() - timer;
+        timer = System.nanoTime();
+        
+        // Res Load
         Access access = new FileAccess(a.toFile(), true);
         ResPayload res = new ResFile(access).getRoot();
         access.close();
         
-        resLoadTime += System.nanoTime() - t;
-        t = System.nanoTime();
+        result.loadTime = System.nanoTime() - timer;
+        timer = System.nanoTime();
         
+        // Res Data
         int structureSize = res.getSizeOfRoot();
         DummyResData resData = new DummyResData();
         res.fillDummyResData(resData);
@@ -123,8 +131,9 @@ public class ARCVFile {
         int dataEntries = resData.getDataEntries();
         resData.close();
         
-        resDataTime += System.nanoTime() - t;
+        result.dataTime = System.nanoTime() - timer;
         
+        // sanity checks, MARV generation
         if (input.length - Utils.align(structureSize, 0x80) != dataSize && dataSize != 0)
             Main.LOGGER.log(Level.WARNING, () -> name + " calculated size and actual size differs. It's format might be invalid, please check and re-export.");
         
@@ -132,6 +141,28 @@ public class ARCVFile {
         if (res.getType() == Payload.GENERIC || res.getType() == Payload.BTX)
             marv = null;
         
-        arcvinfo.addEntry(new VCRAEntry(compressedSize, input.length, name, startSector, marv));
+        result.marv = marv;
+        
+        return result;
+    }
+    
+    static class AddFileResult {
+        ByteArrayOutputStream output;
+        MARVEntry marv;
+        String name;
+        int uncompressedSize;
+        int compressedSize;
+        
+        long zipTime;
+        long loadTime;
+        long dataTime;
+    }
+    
+    static class RunValues {
+        int sectorCount = 0;
+        
+        long zipTime = 0;
+        long resLoadTime = 0;
+        long resDataTime = 0;
     }
 }
