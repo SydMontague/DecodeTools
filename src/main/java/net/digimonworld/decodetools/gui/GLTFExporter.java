67d0049a27b979c05c3d083aeea458bf185d2cd6
Romsstar <romsstar@gmail.com>
2023-04-27 21:00:31 +0200

    Formatted

--- src/main/java/net/digimonworld/decodetools/gui/GLTFExporter.java
+++ src/main/java/net/digimonworld/decodetools/gui/GLTFExporter.java
@@ -67,184 +67,183 @@ import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 import java.nio.file.*;
 
-
 import java.awt.image.*;
 
-
 public class GLTFExporter {
-	   private final HSMPKCAP hsmp;
-	   
-	   public GLTFExporter(HSMPKCAP hsmp) {
-	        this.hsmp = hsmp;
-	    }
+	private final HSMPKCAP hsmp;
 
-	 public void export(File output) throws IOException
-	    { 
+	public GLTFExporter(HSMPKCAP hsmp) {
+		this.hsmp = hsmp;
+	}
 
-		 DefaultGltfModel gltfModel = new DefaultGltfModel();
-    	 GlTF gltf = GltfCreatorV2.create(gltfModel);
-		 
-	         Asset inputAsset = new Asset();
-	         inputAsset.setVersion("2.0");
-	         inputAsset.setGenerator("jgltf-parent-2.0.3");	        
-	         gltf.setAsset(inputAsset);
-		     
-	         int meshId = 0;
-	         for(HSEMPayload hsem : hsmp.getHSEM().getHSEMEntries()) {
-	             Map<Short, Short> currentAssignments = new HashMap<>();
-	             short currentTexture = -1;
-	             
-	             for(HSEMEntry entry : hsem.getEntries()) {
-	                 
-	                 switch(entry.getHSEMType()) {
-	                     case JOINT:
-	                         ((HSEMJointEntry) entry).getJointAssignment().forEach(currentAssignments::put);
-	                         break;
-	                     case TEXTURE:
-	                         currentTexture = ((HSEMTextureEntry) entry).getTextureAssignment().getOrDefault((short) 0, currentTexture);
-	                         break;
-	                     default: break;
-	                 }
-	                 
-	                 if(entry.getHSEMType() != HSEMEntryType.DRAW)
-	                     continue;
+	public void export(File output) throws IOException {
 
-	                 HSEMDrawEntry draw = (HSEMDrawEntry) entry;
-	                 XTVOPayload xtvo = hsmp.getXTVP().get(draw.getVertexId());
-	                 XDIOPayload xdio = hsmp.getXDIP().get(draw.getIndexId());
-	               
-	                 final String meshName = "geom-" + meshId++;
-	     
-	         // Create positions, normals,faces    	 
-             List<Float> positions = vertexAttribToList(xtvo.getVertices(), XTVORegisterType.POSITION);
-	         List<Float> normals = vertexAttribToList(xtvo.getVertices(), XTVORegisterType.NORMAL);
-	         List<Integer> indices = new ArrayList<>();
-	         xdio.getFaces().forEach(face -> {
-	             indices.add(face.getVert1());
-	             indices.add(face.getVert2());
-	             indices.add(face.getVert3());
-	         });	       
-             	         
-	         byte[] posBytes = floatListToByteArray(positions);
-	         byte[] normalBytes = floatListToByteArray(normals);
-	         byte[] faceBytes = intListToByteBuffer(indices);
-	         
-             byte[] combinedData = new byte[posBytes.length + normalBytes.length+faceBytes.length];
-             System.arraycopy(posBytes, 0, combinedData, 0, posBytes.length);
-             System.arraycopy(normalBytes, 0, combinedData, posBytes.length, normalBytes.length);
-             System.arraycopy(faceBytes, 0, combinedData, posBytes.length + normalBytes.length, faceBytes.length);
-	   
-	         Buffer buffer = new Buffer();
-             buffer.setByteLength(combinedData.length);
-             buffer.setUri("data:application/octet-stream;base64,"+ Base64.getEncoder().encodeToString(combinedData));
-              
-             gltf.addBuffers(buffer);
-             
-                 
-          // Create buffer views
-             BufferView posBufferView = new BufferView();
-             posBufferView.setBuffer(0); // use the first buffer
-             posBufferView.setByteOffset(0); // start at the beginning
-             posBufferView.setByteLength(posBytes.length);
-             posBufferView.setTarget(34962);   
-         
-             BufferView normalBufferView = new BufferView();
-             normalBufferView.setBuffer(0); // use the first buffer
-             normalBufferView.setByteOffset(posBufferView.getByteOffset() + posBufferView.getByteLength()); // start where positions left off
-             normalBufferView.setByteLength(normalBytes.length);
-             normalBufferView.setTarget(34962);
-             
-             BufferView indexBufferView = new BufferView();
-             indexBufferView.setBuffer(0); // use the first buffer
-             indexBufferView.setByteOffset(normalBufferView.getByteOffset() + normalBufferView.getByteLength()); // start where normals left off
-             indexBufferView.setByteLength(faceBytes.length);
-             indexBufferView.setTarget(34963);
-             
-          // Add buffer views to GLTF
-             gltf.addBufferViews(posBufferView);
-             gltf.addBufferViews(normalBufferView);
-             gltf.addBufferViews(indexBufferView);
-             
-             
-          // Create accessors
-             Accessor posAccessor = new Accessor();
-             posAccessor.setBufferView(0); // use the first buffer view
-             posAccessor.setComponentType(5126);//FLOAT
-             posAccessor.setCount(positions.size() / 3); // each position has 3 components (x, y, z)
-             posAccessor.setType("VEC3");
+		DefaultGltfModel gltfModel = new DefaultGltfModel();
+		GlTF gltf = GltfCreatorV2.create(gltfModel);
 
-             Accessor normalAccessor = new Accessor();
-             normalAccessor.setBufferView(1); // use the second buffer view
-             normalAccessor.setComponentType(5126);
-             normalAccessor.setCount(normals.size() / 3); // each normal has 3 components (x, y, z)
-             normalAccessor.setType("VEC3");
+		Asset inputAsset = new Asset();
+		inputAsset.setVersion("2.0");
+		inputAsset.setGenerator("jgltf-parent-2.0.3");
+		gltf.setAsset(inputAsset);
 
-             Accessor indexAccessor = new Accessor();
-             indexAccessor.setBufferView(2); // use the third buffer view
-             indexAccessor.setComponentType(5123);
-             indexAccessor.setCount(indices.size());
-             indexAccessor.setType("SCALAR");
-             
-             //Add Accessors
-             gltf.addAccessors(posAccessor);
-             gltf.addAccessors(normalAccessor);
-             gltf.addAccessors(indexAccessor);
-   	               
-             
-        	 Node node = new Node();
-        	 node.setMesh(meshId);
-        	 node.setName(meshName); 
-        	 gltf.addNodes(node); // add the nodes to the glTF model
-        	 
-	         // Create a scene with the node
-	         Scene scene = new Scene();
-	         scene.addNodes(hsmp.getEntryCount());	         
-	         gltf.addScenes(scene);
-	         gltf.setScene(0);
-	 
-	         
-	         Mesh mesh = new Mesh(); 
-	         mesh.setName(meshName);
-	         
-	         
-	         MeshPrimitive primitive = new MeshPrimitive();
-	         primitive.addAttributes("POSITION", 0);
-             primitive.addAttributes("NORMAL", 1);
+		int meshId = 0;
+		for (HSEMPayload hsem : hsmp.getHSEM().getHSEMEntries()) {
+			Map<Short, Short> currentAssignments = new HashMap<>();
+			short currentTexture = -1;
 
-             mesh.addPrimitives(primitive);
+			for (HSEMEntry entry : hsem.getEntries()) {
 
-	         gltf.addMeshes(mesh);         
-	         
-	             }}	
-	         
-	         File outputFile = new File(output, hsmp.getName() + ".gltf");
-		        try (OutputStream os = new FileOutputStream(outputFile)) {
-		            GltfWriter gltfWriter = new GltfWriter();
-		            gltfWriter.write(gltf, os);
-		        } catch (IOException e) {
-		        	e.printStackTrace();
-		        }	    
-		        
-}   public static byte[] floatListToByteArray(List<Float> floatList) {
-    int floatCount = floatList.size();
-    ByteBuffer byteBuffer = ByteBuffer.allocate(floatCount * Float.BYTES);
-    byteBuffer.order(ByteOrder.nativeOrder());
-    for (float f : floatList) {
-        byteBuffer.putFloat(f);
-    }
-    return byteBuffer.array();
-}
-    public static byte[]  intListToByteBuffer(List<Integer> data) {
-        ByteBuffer buffer = ByteBuffer.allocate(data.size() * Integer.BYTES);
-        buffer.order(ByteOrder.nativeOrder());
-        for (int i : data) {
-            buffer.putInt(i);
-        }	       
-        return buffer.array();
-    }
+				switch (entry.getHSEMType()) {
+				case JOINT:
+					((HSEMJointEntry) entry).getJointAssignment().forEach(currentAssignments::put);
+					break;
+				case TEXTURE:
+					currentTexture = ((HSEMTextureEntry) entry).getTextureAssignment().getOrDefault((short) 0,
+							currentTexture);
+					break;
+				default:
+					break;
+				}
 
-    
- private static List<Float> vertexAttribToList(List<XTVOVertex> vertices, XTVORegisterType type) {
-    return vertices.stream().map(a -> a.getParameter(type)).flatMap(a -> a.getValue().stream().map(b -> a.getKey().getValue(b))).collect(Collectors.toList());
-}
+				if (entry.getHSEMType() != HSEMEntryType.DRAW)
+					continue;
+
+				HSEMDrawEntry draw = (HSEMDrawEntry) entry;
+				XTVOPayload xtvo = hsmp.getXTVP().get(draw.getVertexId());
+				XDIOPayload xdio = hsmp.getXDIP().get(draw.getIndexId());
+
+				final String meshName = "geom-" + meshId++;
+
+				// Create positions, normals,faces
+				List<Float> positions = vertexAttribToList(xtvo.getVertices(), XTVORegisterType.POSITION);
+				List<Float> normals = vertexAttribToList(xtvo.getVertices(), XTVORegisterType.NORMAL);
+				List<Integer> indices = new ArrayList<>();
+				xdio.getFaces().forEach(face -> {
+					indices.add(face.getVert1());
+					indices.add(face.getVert2());
+					indices.add(face.getVert3());
+				});
+
+				byte[] posBytes = floatListToByteArray(positions);
+				byte[] normalBytes = floatListToByteArray(normals);
+				byte[] faceBytes = intListToByteBuffer(indices);
+
+				byte[] combinedData = new byte[posBytes.length + normalBytes.length + faceBytes.length];
+				System.arraycopy(posBytes, 0, combinedData, 0, posBytes.length);
+				System.arraycopy(normalBytes, 0, combinedData, posBytes.length, normalBytes.length);
+				System.arraycopy(faceBytes, 0, combinedData, posBytes.length + normalBytes.length, faceBytes.length);
+
+				Buffer buffer = new Buffer();
+				buffer.setByteLength(combinedData.length);
+				buffer.setUri(
+						"data:application/octet-stream;base64," + Base64.getEncoder().encodeToString(combinedData));
+
+				gltf.addBuffers(buffer);
+
+				// Create buffer views
+				BufferView posBufferView = new BufferView();
+				posBufferView.setBuffer(0); // use the first buffer
+				posBufferView.setByteOffset(0); // start at the beginning
+				posBufferView.setByteLength(posBytes.length);
+				posBufferView.setTarget(34962);
+
+				BufferView normalBufferView = new BufferView();
+				normalBufferView.setBuffer(0); // use the first buffer
+				normalBufferView.setByteOffset(posBufferView.getByteOffset() + posBufferView.getByteLength());
+				normalBufferView.setByteLength(normalBytes.length);
+				normalBufferView.setTarget(34962);
+
+				BufferView indexBufferView = new BufferView();
+				indexBufferView.setBuffer(0); // use the first buffer
+				indexBufferView.setByteOffset(normalBufferView.getByteOffset() + normalBufferView.getByteLength());
+				indexBufferView.setByteLength(faceBytes.length);
+				indexBufferView.setTarget(34963);
+
+				// Add buffer views to GLTF
+				gltf.addBufferViews(posBufferView);
+				gltf.addBufferViews(normalBufferView);
+				gltf.addBufferViews(indexBufferView);
+
+				// Create accessors
+				Accessor posAccessor = new Accessor();
+				posAccessor.setBufferView(0); // use the first buffer view
+				posAccessor.setComponentType(5126);// FLOAT
+				posAccessor.setCount(positions.size() / 3); // each position has 3 components (x, y, z)
+				posAccessor.setType("VEC3");
+
+				Accessor normalAccessor = new Accessor();
+				normalAccessor.setBufferView(1); // use the second buffer view
+				normalAccessor.setComponentType(5126);
+				normalAccessor.setCount(normals.size() / 3); // each normal has 3 components (x, y, z)
+				normalAccessor.setType("VEC3");
+
+				Accessor indexAccessor = new Accessor();
+				indexAccessor.setBufferView(2); // use the third buffer view
+				indexAccessor.setComponentType(5123);
+				indexAccessor.setCount(indices.size());
+				indexAccessor.setType("SCALAR");
+
+				// Add Accessors
+				gltf.addAccessors(posAccessor);
+				gltf.addAccessors(normalAccessor);
+				gltf.addAccessors(indexAccessor);
+
+				Node node = new Node();
+				node.setMesh(meshId);
+				node.setName(meshName);
+				gltf.addNodes(node); // add the nodes to the glTF model
+
+				// Create a scene with the node
+				Scene scene = new Scene();
+				scene.addNodes(hsmp.getEntryCount());
+				gltf.addScenes(scene);
+				gltf.setScene(0);
+
+				Mesh mesh = new Mesh();
+				mesh.setName(meshName);
+
+				MeshPrimitive primitive = new MeshPrimitive();
+				primitive.addAttributes("POSITION", 0);
+				primitive.addAttributes("NORMAL", 1);
+
+				mesh.addPrimitives(primitive);
+
+				gltf.addMeshes(mesh);
+
+			}
+		}
+
+		File outputFile = new File(output, hsmp.getName() + ".gltf");
+		try (OutputStream os = new FileOutputStream(outputFile)) {
+			GltfWriter gltfWriter = new GltfWriter();
+			gltfWriter.write(gltf, os);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static byte[] floatListToByteArray(List<Float> floatList) {
+		int floatCount = floatList.size();
+		ByteBuffer byteBuffer = ByteBuffer.allocate(floatCount * Float.BYTES);
+		byteBuffer.order(ByteOrder.LITTLE_ENDIAN);
+		for (float f : floatList) {
+			byteBuffer.putFloat(f);
+		}
+		return byteBuffer.array();
+	}
+
+	public static byte[] intListToByteBuffer(List<Integer> data) {
+		ByteBuffer buffer = ByteBuffer.allocate(data.size() * Integer.BYTES);
+		buffer.order(ByteOrder.LITTLE_ENDIAN);
+		for (int i : data) {
+			buffer.putInt(i);
+		}
+		return buffer.array();
+	}
+
+	private static List<Float> vertexAttribToList(List<XTVOVertex> vertices, XTVORegisterType type) {
+		return vertices.stream().map(a -> a.getParameter(type))
+				.flatMap(a -> a.getValue().stream().map(b -> a.getKey().getValue(b))).collect(Collectors.toList());
+	}
 }
